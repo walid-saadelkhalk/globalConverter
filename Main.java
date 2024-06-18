3e6c57fc188fbf0112a92e29cb6fc1f3bdceed78
Dhjali <barbara.vanni@laplateforme.io>
2024-06-18 16:25:02 +0200

    [ADD] ConvertTo class // add first method for hexadecimal converter

--- Main.java
+++ Main.java
@@ -1,8 +1,17 @@
+import convert.ConvertTo;
+
 public class Main {
 
     public static void main (String[] args) {
         
         text.Text text = new text.Text();
-        System.out.println(text.getText());
+        ConvertTo converter = new ConvertTo();
+
+        String inputText = text.getText();
+
+        String hexResult = converter.toHexadecimal(inputText);
+
+        System.out.println("Hexadecimal: " + hexResult);
+
     }
 }
