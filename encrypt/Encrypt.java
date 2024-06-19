43367b272c3f06955ac4ab996f75f52a06a445db
mathis-serra <mathis.serra@laplateforme.io>
2024-06-19 14:32:43 +0200

    [FIX] fixed the encrypt class and make it make it looks more like the subject

--- encrypt/Encrypt.java
+++ encrypt/Encrypt.java
@@ -1,6 +1,21 @@
 package encrypt;
 
+import java.util.Scanner;
+
 public class Encrypt {
+    private Scanner input = new Scanner(System.in);
+
+    public boolean askForEncryption() {
+        System.out.println("Do you want to encrypt the text before converting to hexadecimal? (yes/no): ");
+        String response = input.nextLine();
+        return response.equalsIgnoreCase("yes");
+    }
+
+    public int getShiftValue() {
+        System.out.println("Enter the shift value: ");
+        return input.nextInt();
+    }
+
     public String encryptText(String text, int shift) {
         StringBuilder encrypted = new StringBuilder();
         for (char c : text.toCharArray()) {
